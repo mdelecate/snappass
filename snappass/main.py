1e9c1f15e9a211376e89f6f44c486e79a38958fb
Mark <mdelecate@users.noreply.github.com>
2020-06-20 01:10:56 +0800

    Combine GET functions
    Combined both "GET" functions index() and preview_password() into a single function, since route() cannot route based on query strings. 
    
    Removed the now unnecessary remainder of the function show_password(), as it was already combined into the handle_password() function in the previous commit.

--- snappass/main.py
+++ snappass/main.py
@@ -154,8 +154,16 @@ def clean_input():
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('set_password.html')
+    if empty(request.args.get('t')):
+        return render_template('set_password.html')
+    else:
+        password_key = request.args.get('t')
+        password_key = url_unquote_plus(password_key)
+        if not password_exists(password_key):
+            abort(404)
 
+    return render_template('preview.html')
+        
 
 @app.route('/', methods=['POST'])
 def handle_password():
@@ -179,21 +187,6 @@ def handle_password():
             abort(404)
 
         return render_template('password.html', password=password)
-        
-
-@app.route('/', methods=['GET'])
-def preview_password():
-    password_key = request.args.get('t')
-    password_key = url_unquote_plus(password_key)
-    if not password_exists(password_key):
-        abort(404)
-
-    return render_template('preview.html')
-
-
-@app.route('/', methods=['POST'])
-def show_password():
-    
 
 
 @check_redis_alive
